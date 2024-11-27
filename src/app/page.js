a6dde22c5f73afa1d021de4d9ec98f2ae53df993
macbook pro <macbookpro@lovie3.local>
2024-11-27 23:19:14 +0800

    business card ehlel

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,14 @@
-"use client"
-import Image from "next/image";
-import"./style.css\
-
+"user client";
+import userData from "./userData";
+import  Component  from "./component.js";
+import React from "react";
+import "./style.css";
 export default function Home() {
-  const [count, setCount] = useState(0);
-  let counter = 0;
-   const handleClick = () => {
-     setCount(count + 1);
-  //   counter++
-  //   console.log(counter)
-  };
   return (
-    <div>{count}
-      <button onClick={handleClick}>click</button>
-      <ul>{productList}</ul>
+    <div className="cardContainer">
+      {userData.map((employer) => {
+        return <Component props={employer} />;
+      })}
     </div>
   );
 }
