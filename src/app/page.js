7c88c7e813d0974e052381b142dace543267eecf
Nurul0727-code <nurul89552017@gmail.com>
2024-11-28 18:10:07 +0800

    props

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,59 @@
 "user client";
 import userData from "./userData";
-import  Component  from "./component.js";
+import Component from "./component.js";
 import React from "react";
 import "./style.css";
+
 export default function Home() {
   return (
     <div className="cardContainer">
-      {userData.map((employer) => {
-        return <Component props={employer} />;
+      {userData.map((user) => {
+        return <Component user={user} />;
       })}
     </div>
   );
 }
+const Greeting = (props) => {
+  return (
+    <div>
+      Hi {props.name} {props.age}
+    </div>
+  );
+};
+
+// export default function Page() {
+//   return (
+//     <>
+//       <Greeting name="Nurkaeinoo" age={17} />
+//       <Greeting name="Naraa" />
+//     </>
+//   );
+// }
+
+// function Avatar(props) {
+//   return <img width={props.size} height={props.size} src={props.link} />;
+// }
+// export default function Profile() {
+//   return (
+//     <>
+//       <Avatar
+//         link={
+//           "https://media.gq-magazine.co.uk/photos/5e442958995fbc00086126ad/16:9/w_2580%2Cc_limit/20191219-BTS-04.jpg"
+//         }
+//         size={300}
+//       />
+//       <Avatar
+//         link={
+//           "https://media.gq-magazine.co.uk/photos/5e442958995fbc00086126ad/16:9/w_2580%2Cc_limit/20191219-BTS-04.jpg"
+//         }
+//         size={200}
+//       />
+//       <Avatar
+//         link={
+//           "https://media.gq-magazine.co.uk/photos/5e442958995fbc00086126ad/16:9/w_2580%2Cc_limit/20191219-BTS-04.jpg"
+//         }
+//         size={250}
+//       />
+//     </>
+//   );
+// }
